795683bf19ddfa4f408dcaa30906cdcc2880e3f9
jake <boxjack.romero@gmail.com>
2021-01-26 12:11:07 -0300

    adjust db relations

--- db/db.go
+++ db/db.go
@@ -77,5 +77,32 @@ func AutoMigrate(db *gorm.DB) error {
 		&models.Rubric{},
 		&models.Review{},
 	).Error
+	db.Model(&models.Career{}).AddForeignKey("department_id", "departments(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Student{}).AddForeignKey("career_id", "careers(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Teacher{}).AddForeignKey("account_id", "accounts(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Admin{}).AddForeignKey("account_id", "accounts(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Link{}).AddForeignKey("link_type_id", "link_types(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Link{}).AddForeignKey("project_id", "projects(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Meet{}).AddForeignKey("channel_id", "channels(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Meet{}).AddForeignKey("project_id", "projects(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Commit{}).AddForeignKey("project_id", "projects(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Milestone{}).AddForeignKey("project_id", "projects(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Progress{}).AddForeignKey("project_id", "projects(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Review{}).AddForeignKey("reviewer_id", "teachers(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Review{}).AddForeignKey("rubric_id", "rubrics(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Review{}).AddForeignKey("project_id", "projects(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Project{}).AddForeignKey("project_state_id", "project_states(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Project{}).AddForeignKey("project_type_id", "project_types(id)", "RESTRICT", "RESTRICT")
+
+	db.Table("project_authors").AddForeignKey("project_id", "projects(id)", "CASCADE", "CASCADE")
+	db.Table("project_authors").AddForeignKey("student_id", "students(id)", "CASCADE", "CASCADE")
+	db.Table("project_guides").AddForeignKey("project_id", "projects(id)", "CASCADE", "CASCADE")
+	db.Table("project_guides").AddForeignKey("teacher_id", "teachers(id)", "CASCADE", "CASCADE")
+	db.Table("project_subjects").AddForeignKey("project_id", "projects(id)", "CASCADE", "CASCADE")
+	db.Table("project_subjects").AddForeignKey("subject_id", "subjects(id)", "CASCADE", "CASCADE")
+
+	if err != nil {
+		return err
+	}
 	return err
 }
